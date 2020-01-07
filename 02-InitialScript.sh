ccc681c890d7971bed65c9b3f6bd51dc40d71585
Johan de Vries <johan.de.vries@mstack.nl>
2020-01-07 21:04:10 +0100

    Start scripts with sudo

--- 02-InitialScript.sh
+++ 02-InitialScript.sh
@@ -17,16 +17,16 @@ cd ~/git/PI_Vericoin/Scripts/
 
 #Run pi_update
 sudo chmod u+x ./pi_update.sh
-./pi_update.sh
+sudo ./pi_update.sh
 
 #Run pi_installeprogramms
 sudo chmod u+x ./pi_installeprogramms.sh
-./pi_installeprogramms.sh
+sudo ./pi_installeprogramms.sh
 
 #Run pi_installdocker
 sudo chmod u+x ./pi_installdocker.sh
-./pi_installdocker.sh
+sudo ./pi_installdocker.sh
 
 #Run pi_setupdockervericoinvolume
 sudo chmod u+x ./pi_setupdockervericoinvolume.sh
-./pi_setupdockervericoinvolume.sh
+sudo ./pi_setupdockervericoinvolume.sh
