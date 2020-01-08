85fab1b76306f37e31ea875df18eb0f786916171
Johan de Vries <johan.de.vries@mstack.nl>
2020-01-08 21:12:35 +0100

    ff

--- Scripts/PI/createdockerfiles.sh
+++ Scripts/PI/createdockerfiles.sh
@@ -4,12 +4,12 @@
 echo "\e[32mCreating docker file directory\e[0m"
 echo "\e[33mCurrent path is: '`pwd`'\e[0m"
 cd ~/
-mkdir -p /home/pi/dockerfiles
+mkdir -p ~/dockerfiles
 
 #Creating Vericoin Docker File
 echo "\e[32mCreating vericoin docker file\e[0m"
 echo "\e[33mCurrent path is: '`pwd`'\e[0m"
-cd /home/pi/dockerfiles/
+cd ~/dockerfiles/
 mkdir -p VericoinDockerFile
-cp -n /home/pi/git/PI_Vericoin/DockerFiles/VericoinDockerFile /home/pi/dockerfiles/VericoinDockerFile/
+cp -n ~/git/PI_Vericoin/DockerFiles/VericoinDockerFile ~/dockerfiles/VericoinDockerFile/
 sudo docker build -t ubuntu1804 ./VericoinDockerFile/
--- Scripts/PI/setupdockervericoinvolume.sh
+++ Scripts/PI/setupdockervericoinvolume.sh
@@ -12,7 +12,7 @@ if [ -f /var/lib/docker/volumes/vericoind_root_home/_data/.vericoinnn/vericoin.c
 		echo "\e[31mvericoin.conf already exsist\e[0m"
 	else
 		echo "\e[32mGetting vericoin.conf\e[0m"
-		cp cd /home/pi/git/PI_Vericoin/Data/vericoin.conf ./
+		cp cd ~/git/PI_Vericoin/Data/vericoin.conf ./
 fi
 
 if [ -f /var/lib/docker/volumes/vericoind_root_home/_data/.vericoinnn/blk0001.dat ]
@@ -48,3 +48,5 @@ fi
 #		wget http://192.168.2.250/PI/Scripts/vericoin/vericoind_stake_start.sh -O vericoind_stake_start.sh
 #		sudo chmod +x vericoind_stake_start.sh
 #fi
+
+cd ~/
