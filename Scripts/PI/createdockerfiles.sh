6aaf802461b6713c4046a5d75f4b1c98db2f4853
Johan de Vries <johan.de.vries@mstack.nl>
2020-01-08 22:42:40 +0100

    Test

--- Scripts/PI/createdockerfiles.sh
+++ Scripts/PI/createdockerfiles.sh
@@ -6,10 +6,25 @@ echo "\e[33mCurrent path is: '`pwd`'\e[0m"
 cd ~/
 mkdir -p ~/dockerfiles
 
+echo "Test5"
+
 #Creating Vericoin Docker File
 echo "\e[32mCreating vericoin docker file\e[0m"
 echo "\e[33mCurrent path is: '`pwd`'\e[0m"
 cd ~/dockerfiles/
+
+echo "Test6"
+
 mkdir -p VericoinDockerFile
+
+echo "Test7"
+
 cp -n ~/git/PI_Vericoin/DockerFiles/VericoinDockerFile ~/dockerfiles/VericoinDockerFile/
+
+
+echo "Test8"
+
 sudo docker build -t ubuntu1804 ./VericoinDockerFile/
+
+
+echo "Test9"
