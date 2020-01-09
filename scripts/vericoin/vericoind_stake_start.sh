164ed1ced1d1f7e8279bbdfde515bb442d5241af
Johan de Vries <johan.de.vries@mstack.nl>
2020-01-09 21:01:54 +0100

    Test

--- scripts/vericoin/vericoind_stake_start.sh
+++ scripts/vericoin/vericoind_stake_start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 vericoind walletpassphrase whahanotgonnasharemypass 5 9999999 true
--- scripts/vericoin/vericoind_startwithstake.sh
+++ scripts/vericoin/vericoind_startwithstake.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 clear
 
-rm ~/.vericoin/debug.log
-rm ~/.vericoin/db.log
-vericoind -daemon -staking -conf=~/.vericoin/vericoin.conf &
-###############vericoind -daemon -staking -conf=~/.vericoin/vericoin.conf -wallet=wallet.dat -walletpassphrase=whahanotgonnasharethat &
-#sleep 1
-#$VERICOIND_PID = $(pidof vericoind)
-#/root/vericoind_monitordebuglog_multitail &
-sleep 150
-#########/root/vericoind_stake_start
+vericoind -daemon -staking -conf=~/.vericoin/vericoin.conf -wallet=wallet.dat -walletpassphrase=whahanotgonnasharethat &
+
+sleep 180
+
+/root/scripts/vericoind_stake_start
+
 sleep infinity
-#wait $VERICOIND_PID
-#echo "Vericoind process terminated..."
-ps x > /root/psx
