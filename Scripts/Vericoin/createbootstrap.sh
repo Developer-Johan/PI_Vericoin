b63db5f3abc516acd58e2ef18ffd398561ac098f
Johan de Vries <johan.de.vries@mstack.nl>
2020-01-09 14:43:35 +0100

    Test

--- Scripts/Vericoin/createbootstrap.sh
+++ Scripts/Vericoin/createbootstrap.sh
@@ -5,10 +5,17 @@ printf "\e[32mCreate bootstrap\e[0m"
 printf "\e[33mCurrent path is: '`pwd`'\e[0m"
 
 cd /root/.vericoin/
+printf "Stopping vericoin"
 vericoind stop
+
 mkdir bootstrap
+printf "Copy blk0001.data"
 cp /root/.vericoin/blk0001.dat /root/.vericoin/bootstrap/
+printf "Copy txleveldb"
 cp -R /root/.vericoin/txleveldb/ /root/data/.vericoin/bootstrap/
+printf "Zipping bootstrap"
 zip -r bootstrap.zip bootstrap
+printf "Remove bootstrap/"
 rm -r bootstrap/
+printf "Reboot"
 sudo reboot now
